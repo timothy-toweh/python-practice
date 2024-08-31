6c8ff191e460685cc2e99801409bf2ecc48d6708
Timothy Toweh <136031096+timothy-toweh@users.noreply.github.com>
2024-08-31 13:30:10 +0100

    practice

---
+++ practice.py
@@ -0,0 +1,11 @@
+# Taking inputs from the user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+favorite_colour = input("Enter your favorite colour: ")
+
+# Generating the output message
+output_message = f"Hello {name}\nYour favorite colour is {favorite_colour}\nIt is a color for people {age} years old."
+
+# Printing the output message
+print(output_message)
+
